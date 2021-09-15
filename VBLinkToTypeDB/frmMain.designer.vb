35e54768fbfdc27208db15601d073fc35a2ab4dc
Viev <victormorgante@viev.com>
2021-09-15 21:29:52 +1000

    - Reinitialising after first successful query tests of DLL

---
+++ VBLinkToTypeDB/frmMain.designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(69, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(136, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "127.0.0.1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Host:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Port:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(69, 38)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(136, 20)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = "1729"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(211, 38)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(577, 244)
+        Me.RichTextBox1.TabIndex = 4
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 73)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(193, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Connect To Server"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 102)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(193, 21)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 129)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(193, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Get All Databases"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 158)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(193, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Create Session With Database"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(252, 12)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(536, 20)
+        Me.TextBox3.TabIndex = 9
+        Me.TextBox3.Text = "match $tra (traveler: $per) isa travel; (located: $tra, location: $loc) isa local" &
+    "isation; $loc has name ""French Lick""; $per has full-name $fn; get $fn;"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(208, 15)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Query:"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 259)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(193, 23)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "Execute Match Query"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 292)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "frmMain"
+        Me.Text = "VB Link for TypeDB"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button4 As Button
+End Class
---
+++ VBLinkToTypeDB/frmMain.vb
@@ -0,0 +1,108 @@
+﻿Imports GrpcServer
+Imports TypeDBCustom
+
+Public Class frmMain
+
+    'this is the client we build in C# 
+    Private client As CoreClient = Nothing
+
+    Private Sub AddValuesToTextBox(msg As String)
+        If Me.InvokeRequired Then
+            Me.Invoke(Sub() AddValuesToTextBox(msg))
+            Return
+        End If
+
+        RichTextBox1.AppendText(msg)
+        RichTextBox1.AppendText(vbCrLf)
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'make connection to server
+        client = New CoreClient(TextBox1.Text, CInt(TextBox2.Text))
+        RichTextBox1.Text = $"Successfully connected to server on {TextBox1.Text}:{TextBox2.Text}"
+        RichTextBox1.AppendText(vbCrLf)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        RichTextBox1.AppendText(vbCrLf)
+        'get all databases and add it to combobox
+        ComboBox1.Items.AddRange(client.GetAllDatabases())
+        If ComboBox1.Items.Count >= 1 Then
+            ComboBox1.SelectedIndex = 0
+        End If
+        For Each dbName In ComboBox1.Items
+            RichTextBox1.AppendText($"found databse with name ""{dbName}""")
+            RichTextBox1.AppendText(vbCrLf)
+        Next
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        'connect to selected database from combo box and create session + pulse automatically.
+        Call client.OpenDatabase(ComboBox1.SelectedItem.ToString())
+        RichTextBox1.AppendText($"session successfully opened for database ""{ComboBox1.SelectedItem}""")
+        RichTextBox1.AppendText(vbCrLf)
+        RichTextBox1.AppendText(vbCrLf)
+
+    End Sub
+
+    Private Async Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Try
+
+
+            'this is how we setup a match query, for different query type you have to use different property of query object
+            Dim query As New QueryManager.Types.Req() With {
+            .MatchReq = New QueryManager.Types.Match.Types.Req() With {.Query = TextBox3.Text},
+            .Options = New Options() With {.Parallel = True}
+        }
+
+            'clear the existing transactions if there are any.
+            client.transactionClient.Reqs.Clear()
+            'you can add multiple transaction queries at once
+            client.transactionClient.Reqs.Add(New Transaction.Types.Req() With {.QueryManagerReq = query, .ReqId = client.SessionID})
+            'write the transaction to bi-directional stream
+            Await client.Transactions.RequestStream.WriteAsync(client.transactionClient)
+
+            Dim ServerResp As Transaction.Types.Server = Nothing
+            'this is like an enumrator, you have to call MoveNext for every chunk of data you will receive
+            Do While Await client.Transactions.ResponseStream.MoveNext(Threading.CancellationToken.None)
+                ServerResp = client.Transactions.ResponseStream.Current 'set the current enumrator object to local so can access it shortly
+
+                'this is check if the stream have done sending the data and we exit the loop,
+                'if this miss you will stuck on MoveNext 
+                If ServerResp.ResPart.ResCase = Transaction.Types.ResPart.ResOneofCase.StreamResPart AndAlso
+                    ServerResp.ResPart.StreamResPart.State = Transaction.Types.Stream.Types.State.Done Then
+                    Exit Do
+                End If
+
+                'this will be different according to your scenario. you need to use breakpoint to see data
+                'to implement better logic here. "Answers" below is the array of ConceptMap
+                For Each itm In ServerResp.ResPart.QueryManagerResPart.MatchResPart.Answers.ToArray()
+                    Dim cncpt As Concept = Nothing 'this will be used to get the concept from conceptMap so you can access values.
+                    itm.Map.TryGetValue("fn", cncpt) 'you can get the maping key from your query
+                    AddValuesToTextBox($"[{DateTime.Now}] received response: {cncpt.Thing.Value.String}")
+                Next
+
+            Loop
+
+        Catch ex As Exception
+            Dim lsMessage As String
+            Dim mb As MethodBase = MethodInfo.GetCurrentMethod()
+
+            lsMessage = "Error: " & mb.ReflectedType.Name & "." & mb.Name
+            lsMessage &= vbCrLf & vbCrLf & ex.Message
+            MsgBox(lsMessage)
+        End Try
+
+    End Sub
+End Class
